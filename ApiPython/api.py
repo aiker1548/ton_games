b4421e7035ce0b811e450a660c1166d7b00c57b3
kotov anton <fynjybq_njyz5@mail.ru>
2024-07-18 14:03:55 +0800

    Апи проекта на питоне

--- ApiPython/api.py
+++ ApiPython/api.py
@@ -57,9 +57,9 @@ class GamePlatformApi:
     def __init__(self, API_URL='http://127.0.0.1:8000/api/'):
         self.API_URL = API_URL
 
-    def make_request(self, method, endpoint, token=None, data=None):
+    def __make_request(self, method, endpoint, token=None, data=None):
         url = f'{self.API_URL}{endpoint}'
-        headers = self.get_headers(token) if token else {}
+        headers = self.__get_headers(token) if token else {}
         
         response = requests.request(method, url, headers=headers, json=data)
         logger.info(f"Request {method} {url} returned status {response.status_code}")
@@ -69,7 +69,7 @@ class GamePlatformApi:
             logger.error(f"Request {method} {url} failed with status {response.status_code}: {response.text}")
             response.raise_for_status()
     
-    def get_headers(self, token):
+    def __get_headers(self, token):
         return {
             "Content-Type": "application/json",
             "Authorization": f"Bearer {token}"
@@ -83,32 +83,32 @@ class GamePlatformApi:
             'wallet_address': wallet_address
         }
         
-        user_data = self.make_request('POST', 'users/register/', data=data)
+        user_data = self.__make_request('POST', 'users/register/', data=data)
         return user_data
 
     def login_user(self, username, password) -> str:
         data = {'username': username, 'password': password}
-        json_response = self.make_request('POST', 'users/token/', data=data)
+        json_response = self.__make_request('POST', 'users/token/', data=data)
         return json_response.get('access')
 
     def room_create(self, token: str) -> Room:
-        room_data = self.make_request('POST', 'rooms/create-room/', token)
+        room_data = self.__make_request('POST', 'rooms/create-room/', token)
         room = Room(room_id=room_data['id'], creator=User(room_data['creator']['username'], room_data['creator']['id']))
         return room
 
     def room_leave(self, token: str, id_room: int) -> dict:
-        return self.make_request('POST', f'rooms/{id_room}/leave-room/', token)
+        return self.__make_request('POST', f'rooms/{id_room}/leave-room/', token)
 
     def room_join(self, token: str, id_room: int) -> dict:
-        return self.make_request('POST', f'rooms/{id_room}/join-room/', token)
+        return self.__make_request('POST', f'rooms/{id_room}/join-room/', token)
 
     def get_users_in_room(self, token: str, id_room: int) -> list[User]:
-        users_data = self.make_request('GET', f'rooms/{id_room}/users/', token)
+        users_data = self.__make_request('GET', f'rooms/{id_room}/users/', token)
         users = [User(user['username'], user['id']) for user in users_data]
         return users
 
     def get_rooms(self, token: str) -> list[Room]:
-        rooms_data = self.make_request('GET', 'rooms/', token)
+        rooms_data = self.__make_request('GET', 'rooms/', token)
         rooms = []
         
         for room_json in rooms_data:
@@ -126,7 +126,7 @@ class GamePlatformApi:
             'bet': bet,
             'choices': choices.get_data()
         }
-        return self.make_request('POST', 'games/create-game/', token, data)
+        return self.__make_request('POST', 'games/create-game/', token, data)
     
     
 
